97e4675e0ea5014fff39626eeeebd1e8f7bccd31
Gwen Mittertreiner <gwen.mittertreiner@gmail.com>
2019-03-14 16:16:47 -0700

    Windows: Pass the Environment to Subprocesses

--- unittests/Basic/POSIXEnvironmentTest.cpp
+++ unittests/Basic/POSIXEnvironmentTest.cpp
@@ -18,15 +18,17 @@ using namespace llbuild;
 using namespace llbuild::basic;
 
 namespace {
-  TEST(POSIXEnvironmentTest, basic) {
-    POSIXEnvironment env;
-    env.setIfMissing("a", "aValue");
-    env.setIfMissing("b", "bValue");
-    env.setIfMissing("a", "NOT HERE");
+#if !defined(_WIN32)
+TEST(POSIXEnvironmentTest, basic) {
+  POSIXEnvironment env;
+  env.setIfMissing("a", "aValue");
+  env.setIfMissing("b", "bValue");
+  env.setIfMissing("a", "NOT HERE");
 
-    auto result = env.getEnvp();
-    EXPECT_EQ(StringRef(result[0]), "a=aValue");
-    EXPECT_EQ(StringRef(result[1]), "b=bValue");
-    EXPECT_EQ(result[2], nullptr);
+  auto result = env.getEnvp();
+  EXPECT_EQ(StringRef(result[0]), "a=aValue");
+  EXPECT_EQ(StringRef(result[1]), "b=bValue");
+  EXPECT_EQ(result[2], nullptr);
   }
+#endif
 }
