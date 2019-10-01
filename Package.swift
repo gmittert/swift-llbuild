e12e79e733b5a82096fb62d687c72f8026fef3c2
Gwen Mittertreiner <gwen.mittertreiner@gmail.com>
2019-10-01 16:16:43 -0700

    Modified Package.swift to build on Windows

--- Package.swift
+++ Package.swift
@@ -27,14 +27,26 @@ let package = Package(
         .target(
             name: "llbuild",
             dependencies: ["llbuildCommands"],
-            path: "products/llbuild"
+            path: "products/llbuild",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ],
+            linkerSettings: [.linkedLibrary("Shlwapi", .when(platforms: [.windows])), .linkedLibrary("swiftCore")]
         ),
 
         /// The custom build tool used by the Swift package manager.
         .target(
             name: "swift-build-tool",
             dependencies: ["llbuildBuildSystem"],
-            path: "products/swift-build-tool"
+            path: "products/swift-build-tool",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ],
+            linkerSettings: [.linkedLibrary("Shlwapi"), .linkedLibrary("swiftCore")]
         ),
 
         /// The custom build tool used by the Swift package manager.
@@ -42,14 +54,26 @@ let package = Package(
             name: "llbuildSwift",
             dependencies: ["libllbuild"],
             path: "products/llbuildSwift",
-            exclude: []
+            exclude: [],
+            cxxSettings: [
+              .define("libllbuild_EXPORTS", .when(platforms: [.windows])),
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ]
         ),
 
         /// The public llbuild API.
         .target(
             name: "libllbuild",
             dependencies: ["llbuildCore", "llbuildBuildSystem"],
-            path: "products/libllbuild"
+            path: "products/libllbuild",
+            cxxSettings: [
+              .define("libllbuild_EXPORTS", .when(platforms: [.windows])),
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ]
         ),
 
         // MARK: Components
@@ -57,28 +81,53 @@ let package = Package(
         .target(
             name: "llbuildBasic",
             dependencies: ["llvmSupport"],
-            path: "lib/Basic"
+            path: "lib/Basic",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ]
         ),
         .target(
             name: "llbuildCore",
             dependencies: ["llbuildBasic"],
             path: "lib/Core",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ],
             linkerSettings: [.linkedLibrary("sqlite3")]
         ),
         .target(
             name: "llbuildBuildSystem",
             dependencies: ["llbuildCore"],
-            path: "lib/BuildSystem"
+            path: "lib/BuildSystem",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ]
         ),
         .target(
             name: "llbuildNinja",
             dependencies: ["llbuildBasic"],
-            path: "lib/Ninja"
+            path: "lib/Ninja",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ]
         ),
         .target(
             name: "llbuildCommands",
             dependencies: ["llbuildCore", "llbuildBuildSystem", "llbuildNinja"],
-            path: "lib/Commands"
+            path: "lib/Commands",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ]
         ),
 
         // MARK: Test Targets
@@ -86,23 +135,65 @@ let package = Package(
         .target(
             name: "llbuildBasicTests",
             dependencies: ["llbuildBasic", "gtest"],
-            path: "unittests/Basic"),
+            path: "unittests/Basic",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+              .unsafeFlags(["-Wno-modules-import-nested-redundant"]),
+            ],
+            linkerSettings: [.linkedLibrary("Shlwapi", .when(platforms: [.windows])), .linkedLibrary("swiftCore")]
+        ),
         .target(
             name: "llbuildCoreTests",
             dependencies: ["llbuildCore", "gtest"],
-            path: "unittests/Core"),
+            path: "unittests/Core",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+              .unsafeFlags(["-Wno-modules-import-nested-redundant"]),
+            ],
+            linkerSettings: [.linkedLibrary("Shlwapi", .when(platforms: [.windows])), .linkedLibrary("swiftCore")]
+        ),
+
         .target(
             name: "llbuildBuildSystemTests",
             dependencies: ["llbuildBuildSystem", "gtest"],
-            path: "unittests/BuildSystem"),
+            path: "unittests/BuildSystem",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+              .unsafeFlags(["-Wno-modules-import-nested-redundant"]),
+            ],
+            linkerSettings: [.linkedLibrary("Shlwapi", .when(platforms: [.windows])), .linkedLibrary("swiftCore")]
+        ),
+
         .target(
             name: "llbuildNinjaTests",
             dependencies: ["llbuildNinja", "gtest"],
-            path: "unittests/Ninja"),
+            path: "unittests/Ninja",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+              .unsafeFlags(["-Wno-modules-import-nested-redundant"]),
+            ],
+            linkerSettings: [.linkedLibrary("Shlwapi", .when(platforms: [.windows])), .linkedLibrary("swiftCore")]
+        ),
+
         .testTarget(
             name: "llbuildSwiftTests",
             dependencies: ["llbuildSwift"],
-            path: "unittests/Swift"),
+            path: "unittests/Swift",
+            cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+              .unsafeFlags(["-Wno-modules-import-nested-redundant"]),
+            ]
+        ),
         
         // MARK: GoogleTest
 
@@ -117,18 +208,33 @@ let package = Package(
                 "gtest-test-part.cc",
                 "gtest-typed-test.cc",
                 "gtest.cc",
-            ]),
+            ],
+            cxxSettings: [
+                .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+                .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ]
+        ),
         
         // MARK: Ingested LLVM code.
         .target(
           name: "llvmDemangle",
-          path: "lib/llvm/Demangle"
+          path: "lib/llvm/Demangle",
+          cxxSettings: [
+              .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+              .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+              .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+          ]
         ),
 
         .target(
             name: "llvmSupport",
             dependencies: ["llvmDemangle"],
             path: "lib/llvm/Support",
+            cxxSettings: [
+                .define("LLVM_ON_WIN32", .when(platforms: [.windows])),
+                .define("_CRT_SECURE_NO_WARNINGS", .when(platforms: [.windows])),
+                .define("_CRT_NONSTDC_NO_DEPRECATE", .when(platforms: [.windows])),
+            ],
             linkerSettings: [.linkedLibrary("ncurses", .when(platforms: [.linux, .macOS]))]
         ),
     ],
